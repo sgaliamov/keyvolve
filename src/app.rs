8c11c88e11892c9274a7b37519354e4a703a6b24
s. galiamov <6alyamov@gmail.com>
2026-04-30 22:15:47 +0200

    cases

--- src/app.rs
+++ src/app.rs
@@ -14,8 +14,6 @@ pub fn run(config: Option<Config>, app: AppHandle) -> Result<()> {
     let cfg = config.wrap_err("Missing config.")?;
     trace!("Starting with config: {:#?}", cfg);
 
-    let keyboard = Keyboard::load(cfg.keyboard.unwrap())?;
-
     let words = std::fs::read_to_string(cfg.text.unwrap())
         .into_diagnostic()
         .wrap_err("Failed to read text file")?
@@ -24,54 +22,72 @@ pub fn run(config: Option<Config>, app: AppHandle) -> Result<()> {
         .collect::<Vec<_>>();
     let words_ref = words.iter().map(|s| s.as_str()).collect_vec();
 
+    let layouts_path = cfg.layouts.wrap_err("Missing layouts path in config")?;
+    let layouts = Layout::load(&layouts_path);
+    info!("Loaded {} layouts", layouts.len());
+
     match cfg.mode {
         Mode::Evaluate => {
-            let evaluator = LayoutEvaluator::new(&keyboard);
-
-            let layouts_path = cfg.layouts.unwrap();
-            let layouts = Layout::load(&layouts_path);
-            info!("Loaded {} layouts", layouts.len());
-
-            let mut scored: Vec<_> = layouts
-                .par_iter()
-                .filter_map(|layout| {
-                    if app.should_finish() {
-                        return None;
-                    }
-                    Some((layout, evaluator.score_corpus(&words_ref, &layout.keys)))
-                })
-                .collect();
-
-            scored.sort_by(|a, b| {
-                a.1.fitness
-                    .partial_cmp(&b.1.fitness)
-                    .unwrap_or(std::cmp::Ordering::Equal)
-            });
-
-            scored.iter().take(10).for_each(|(layout, layout_score)| {
-                info!("{} {}", layout.name, layout_score);
-            });
-
-            let mut file = std::fs::File::create(&layouts_path)
-                .into_diagnostic()
-                .wrap_err("Failed to open layouts file for writing")?;
-
-            writeln!(
-                file,
-                "layout_1;layout_2;layout_3;layout_4;layout_5;layout_6;{}",
-                ScoreResult::csv_header()
-            )
-            .into_diagnostic()
-            .wrap_err("Failed to write evaluated layouts header")?;
-
-            for (layout, layout_score) in scored {
-                writeln!(file, "{};{}", layout.name, layout_score.to_csv())
-                    .into_diagnostic()
-                    .wrap_err("Failed to write evaluated layouts")?;
-            }
+            let keyboard = Keyboard::load(cfg.keyboard.unwrap())?;
+            evaluate(keyboard, words_ref, &layouts, &layouts_path, app)?;
         }
-        _ => unimplemented!("Only evaluation mode is implemented currently."),
+        Mode::Optimize => {
+            optimize(words_ref, &layouts, cfg.ga, app)?;
+        }
+        _ => unimplemented!("Only evaluation and optimization modes are implemented currently."),
     }
 
     Ok(())
+}
+
+fn optimize(
+    words: Vec<&str>,
+    layouts: &[Layout],
+    ga: darwin::Config<crate::models::KeyPos>,
+    app: AppHandle,
+) -> Result<()> {
+    todo!()
+}
+
+fn evaluate(
+    keyboard: Keyboard,
+    words: Vec<&str>,
+    layouts: &Vec<Layout>,
+    layouts_path: impl AsRef<std::path::Path>,
+    app: AppHandle,
+) -> Result<()> {
+    let evaluator = LayoutEvaluator::new(&keyboard);
+
+    let mut scored: Vec<_> = layouts
+        .par_iter()
+        .filter_map(|layout| {
+            if app.should_finish() {
+                return None;
+            }
+            Some((layout, evaluator.score_corpus(&words, &layout.keys)))
+        })
+        .collect();
+    scored.sort_by(|a, b| {
+        a.1.fitness
+            .partial_cmp(&b.1.fitness)
+            .unwrap_or(std::cmp::Ordering::Equal)
+    });
+    scored.iter().take(10).for_each(|(layout, layout_score)| {
+        info!("{} {}", layout.name, layout_score);
+    });
+    let mut file = std::fs::File::create(&layouts_path)
+        .into_diagnostic()
+        .wrap_err("Failed to open layouts file for writing")?;
+    writeln!(
+        file,
+        "layout_1;layout_2;layout_3;layout_4;layout_5;layout_6;{}",
+        ScoreResult::csv_header()
+    )
+    .into_diagnostic()
+    .wrap_err("Failed to write evaluated layouts header")?;
+    Ok(for (layout, layout_score) in scored {
+        writeln!(file, "{};{}", layout.name, layout_score.to_csv())
+            .into_diagnostic()
+            .wrap_err("Failed to write evaluated layouts")?;
+    })
 }
