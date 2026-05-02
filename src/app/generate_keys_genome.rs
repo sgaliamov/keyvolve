3f019a8efd4336e7e50b3e3875ca4e8e7dce31cf
s. galiamov <6alyamov@gmail.com>
2026-05-02 21:21:39 +0200

    generate_keys_genome

--- src/app/generate_keys_genome.rs
+++ src/app/generate_keys_genome.rs
@@ -1,6 +1,14 @@
-use crate::models::{KeysGenome, GaContext};
+use crate::models::{GaContext, KeyPos, KeysGenome};
+use itertools::Itertools;
+use rand::seq::SliceRandom;
 
 /// Generate a genome for optimization.
 pub fn generate_keys_genome(_: &GaContext) -> KeysGenome {
-    todo!()
+    let mut chars: Vec<char> = ('a'..='z').collect();
+    chars.shuffle(&mut rand::rng());
+    chars
+        .into_iter()
+        .enumerate()
+        .map(|(i, c)| KeyPos(c, i as u8))
+        .collect_vec()
 }
